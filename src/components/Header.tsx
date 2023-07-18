9635e4a3625a3740de346ee53f3477c64168a0fa
Anna Escolaro <annaescolaro@hotmail.com>
2023-07-18 18:03:07 -0300

    requisito 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,17 @@
+import { useSelector } from 'react-redux';
+import { UserType } from '../types';
+
 function Header() {
-  return <div>Header</div>;
+  const email = useSelector((state: UserType) => state.user.email);
+
+  return (
+    <>
+      <h1>TrybeWallet</h1>
+      <p data-testid="email-field">{ email }</p>
+      <p data-testid="total-field">0</p>
+      <p data-testid="header-currency-field">BRL</p>
+    </>
+  );
 }
 
 export default Header;
