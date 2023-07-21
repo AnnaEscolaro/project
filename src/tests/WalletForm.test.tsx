c0228a9d20ef51a3e89af782c12c9ff1371789b0
Anna Escolaro <annaescolaro@hotmail.com>
2023-07-21 18:50:45 -0300

    requisito 4

--- src/tests/WalletForm.test.tsx
+++ src/tests/WalletForm.test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/dom';
+import { vi } from 'vitest';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,74 +7,95 @@ import userEvent from '@testing-library/user-event';
 import store from '../redux';
 import WalletForm from '../components/WalletForm';
 import App from '../App';
+import mockData from './helpers/mockData';
 
-test('Se é renderizado um input de valor', () => {
-  render(
-    <Provider store={ store }>
-      <WalletForm />
-    </Provider>,
-  );
-  const valueInput = screen.getByTestId('value-input');
-  expect(valueInput).toBeInTheDocument();
-});
-
-test('Se é renderizado um input de descrição', () => {
-  render(
-    <Provider store={ store }>
-      <WalletForm />
-    </Provider>,
-  );
-  const descriptionInput = screen.getByTestId('description-input');
-  expect(descriptionInput).toBeInTheDocument();
-});
-
-test('Se é renderizado um input de categorias', () => {
-  render(
-    <Provider store={ store }>
-      <WalletForm />
-    </Provider>,
-  );
-  const categoryInput = screen.getByText('Alimentação');
-  expect(categoryInput).toBeInTheDocument();
-});
-
-test('Se é renderizado um input de métodos de pagamento', () => {
-  render(
-    <Provider store={ store }>
-      <WalletForm />
-    </Provider>,
-  );
-  const methodInput = screen.getByText('Dinheiro');
-  expect(methodInput).toBeInTheDocument();
-});
-
-test('Se é renderizado um botão de adicionar despesa', () => {
-  render(
-    <Provider store={ store }>
-      <WalletForm />
-    </Provider>,
-  );
-  const button = screen.getByRole('button', { name: 'Adicionar despesa' });
-  expect(button).toBeInTheDocument();
-});
-
-test('Preenchimento do formulário', async () => {
-  render(
-    <BrowserRouter>
+describe('teste', () => {
+  test('Se é renderizado um input de valor', () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => (mockData),
+    });
+    render(
       <Provider store={ store }>
-        <App />
-      </Provider>
-    </BrowserRouter>,
-  );
-  await userEvent.type(screen.getByPlaceholderText('E-mail'), 'aluno@trybe.com');
-  await userEvent.type(screen.getByPlaceholderText('Senha'), 'alunotrybe');
-  const button = screen.getByRole('button', { name: 'Entrar' });
-  expect(button).not.toBeDisabled();
-  await userEvent.click(button);
-  expect(screen.getByText('BRL')).toBeInTheDocument();
-  await userEvent.type(screen.getByTestId('description-input'), 'despesa');
-  await userEvent.type(screen.getByTestId('value-input'), '1');
-  const buttonAdd = screen.getByRole('button', { name: 'Adicionar despesa' });
-  await userEvent.click(buttonAdd);
-  expect(screen.getByTestId('total-field')).not.toBe(0.00);
+        <WalletForm />
+      </Provider>,
+    );
+    const valueInput = screen.getByTestId('value-input');
+    expect(valueInput).toBeInTheDocument();
+  });
+
+  test('Se é renderizado um input de descrição', () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => (mockData),
+    });
+    render(
+      <Provider store={ store }>
+        <WalletForm />
+      </Provider>,
+    );
+    const descriptionInput = screen.getByTestId('description-input');
+    expect(descriptionInput).toBeInTheDocument();
+  });
+
+  test('Se é renderizado um input de categorias', () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => (mockData),
+    });
+    render(
+      <Provider store={ store }>
+        <WalletForm />
+      </Provider>,
+    );
+    const categoryInput = screen.getByText('Alimentação');
+    expect(categoryInput).toBeInTheDocument();
+  });
+
+  test('Se é renderizado um input de métodos de pagamento', () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => (mockData),
+    });
+    render(
+      <Provider store={ store }>
+        <WalletForm />
+      </Provider>,
+    );
+    const methodInput = screen.getByText('Dinheiro');
+    expect(methodInput).toBeInTheDocument();
+  });
+
+  test('Se é renderizado um botão de adicionar despesa', () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => (mockData),
+    });
+    render(
+      <Provider store={ store }>
+        <WalletForm />
+      </Provider>,
+    );
+    const button = screen.getByRole('button', { name: 'Adicionar despesa' });
+    expect(button).toBeInTheDocument();
+  });
+
+  test('Preenchimento do formulário', async () => {
+    global.fetch = vi.fn().mockResolvedValue({
+      json: async () => (mockData),
+    });
+    render(
+      <BrowserRouter>
+        <Provider store={ store }>
+          <App />
+        </Provider>
+      </BrowserRouter>,
+    );
+    await userEvent.type(screen.getByPlaceholderText('E-mail'), 'aluno@trybe.com');
+    await userEvent.type(screen.getByPlaceholderText('Senha'), 'alunotrybe');
+    const button = screen.getByRole('button', { name: 'Entrar' });
+    expect(button).not.toBeDisabled();
+    await userEvent.click(button);
+    expect(screen.getByText('BRL')).toBeInTheDocument();
+    await userEvent.type(screen.getByTestId('description-input'), 'despesa');
+    await userEvent.type(screen.getByTestId('value-input'), '1');
+    const buttonAdd = screen.getByRole('button', { name: 'Adicionar despesa' });
+    await userEvent.click(buttonAdd);
+    expect(screen.getByTestId('total-field')).not.toBe(0.00);
+  });
 });
