3e12e58b878fb9b96b9aa7b5f82567d692eab25b
Anna Escolaro <annaescolaro@hotmail.com>
2023-07-21 18:01:37 -0300

    iniciando requisito 5

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,39 @@
+import { useSelector } from 'react-redux';
+
 function Table() {
-  return <div>Table</div>;
+  const expenses = useSelector(
+    (state: any) => state.wallet.expenses,
+  );
+
+  return (
+    <table>
+      <tr>
+        <th>Descrição</th>
+        <th>Tag</th>
+        <th>Método de pagamento</th>
+        <th>Valor</th>
+        <th>Moeda</th>
+        <th>Câmbio utilizado</th>
+        <th>Valor convertido</th>
+        <th>Moeda de conversão</th>
+        <th>Editar/Excluir</th>
+      </tr>
+      {expenses.map((expense: any) => (
+        <tr key={ expense.id }>
+          <td>{ expense.description }</td>
+          <td>{ expense.tag }</td>
+          <td>{ expense.method }</td>
+          <td>{ expense.value }</td>
+          {/* <td>{ expense.currency }</td> */}
+          {/* <td>{ expense.exchangeRates.expense.currency }</td> */}
+          {/* <td>{ valorConvertido }</td> */}
+          <td>Real</td>
+        </tr>
+      ))}
+
+    </table>
+
+  );
 }
 
 export default Table;
