7a806b62f984ffc68c1f091d3e74524fbfa27a71
Anna Escolaro <annaescolaro@hotmail.com>
2023-07-22 11:17:24 -0300

    requisito 5

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -7,30 +7,37 @@ function Table() {
 
   return (
     <table>
-      <tr>
-        <th>Descrição</th>
-        <th>Tag</th>
-        <th>Método de pagamento</th>
-        <th>Valor</th>
-        <th>Moeda</th>
-        <th>Câmbio utilizado</th>
-        <th>Valor convertido</th>
-        <th>Moeda de conversão</th>
-        <th>Editar/Excluir</th>
-      </tr>
-      {expenses.map((expense: any) => (
-        <tr key={ expense.id }>
-          <td>{ expense.description }</td>
-          <td>{ expense.tag }</td>
-          <td>{ expense.method }</td>
-          <td>{ expense.value }</td>
-          {/* <td>{ expense.currency }</td> */}
-          {/* <td>{ expense.exchangeRates.expense.currency }</td> */}
-          {/* <td>{ valorConvertido }</td> */}
-          <td>Real</td>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
         </tr>
-      ))}
-
+      </thead>
+      <tbody>
+        {expenses.map((expense: any) => (
+          <tr key={ expense.id }>
+            <td>{ expense.description }</td>
+            <td>{ expense.tag }</td>
+            <td>{ expense.method }</td>
+            <td>{ Number(expense.value).toFixed(2) }</td>
+            <td>{ expense.exchangeRates[expense.currency].name }</td>
+            <td>{ Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }</td>
+            <td>
+              { (Number(expense.value)
+              * Number(expense.exchangeRates[expense.currency].ask))
+                .toFixed(2) }
+            </td>
+            <td>Real</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
 
   );
